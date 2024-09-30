4c8fbd3f132c40ccf970f82fe7d0017982fceb29
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2024-09-30 18:51:15 +0530

    fourth commit but its not working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http=require("http")
 const fs=require("fs")
+const path=require("path")
 const { buffer } = require("stream/consumers")
 const myserver=http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html')
@@ -9,27 +10,43 @@ const myserver=http.createServer((req,res)=>{
 
     if(url==="/")
     {
+        
+        fs.readFile("message.txt",{encoding:"utf-8"},(err)=>{
+            if(err)
+            {
+                console.log(err)
+            }
+        })
+        console.log(`data from file ${data}`)
         res.write("<html>")
         res.write("<head><title>Enter message </title></head>")
+
+        res.write(`<body><h1>${data}</h1></body>`)
+        
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
         res.write("</html>")
         return res.end()
     }
-    if(url==="/message" && method==="POST")
+    else if(url==="/message" && method==="POST")
     {
         const body=[]
         req.on('data',(chunk)=>{
-            console.log(chunk)
+            //console.log(chunk)
             body.push(chunk)
         })
-        req.on('end',()=>{
+        return req.on('end',()=>{
             const parsedbody=Buffer.concat(body).toString()
             const message=parsedbody.split("=")[1]
-            fs.writeFileSync("message.txt",message)
-        })
-        res.statusCode=302
-        res.setHeader("Location","/")
-        return res.end()
+            fs.writeFile("message.txt",message,(err)=>{
+                if(err)
+                {
+                    console.log(err)
+                }
+                res.statusCode=302
+                res.setHeader("Location","/")
+                return res.end()
+            })
+        })        
     }
     res.write("<html>")
     res.write("<head><title>My first node server withy res</title></head>")
